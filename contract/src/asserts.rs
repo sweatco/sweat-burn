83a14e593f3c5eaf9c166105eaed6c01c9453fd5
Vasily Styagov <styagov.dev@gmail.com>
2023-07-04 14:49:09 +0100

    Change authorized accounts list to a single manager account

--- contract/src/asserts.rs
+++ contract/src/asserts.rs
@@ -6,8 +6,7 @@ impl Contract {
     pub(crate) fn assert_authorized(&self) {
         let predecessor = env::predecessor_account_id();
         assert!(
-            predecessor == env::current_account_id()
-                || self.authorized_accounts.contains(&predecessor),
+            predecessor == env::current_account_id() || predecessor == self.manager_account_id,
             "Unathorized"
         );
     }
--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,5 +1,4 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::UnorderedSet;
 use near_sdk::json_types::U128;
 use near_sdk::serde_json::json;
 use near_sdk::{
@@ -12,7 +11,7 @@ mod asserts;
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
     token_account_id: AccountId,
-    authorized_accounts: UnorderedSet<AccountId>,
+    manager_account_id: AccountId,
 }
 
 #[ext_contract(ext_self)]
@@ -20,21 +19,13 @@ trait SelfCallbacks {
     fn after_burn(&self, amount: U128) -> U128;
 }
 
-pub trait Authorization {
-    fn add_authorized_account(&mut self, account_id: AccountId);
-
-    fn remove_authorized_account(&mut self, account_id: AccountId);
-
-    fn get_authorized_accounts(&self) -> Vec<AccountId>;
-}
-
 #[near_bindgen]
 impl Contract {
     #[init]
-    pub fn new(token_account_id: AccountId) -> Self {
+    pub fn new(token_account_id: AccountId, manager_account_id: AccountId,) -> Self {
         Self {
             token_account_id,
-            authorized_accounts: UnorderedSet::new(b"a"),
+            manager_account_id,
         }
     }
 
@@ -53,30 +44,6 @@ impl Contract {
                     .with_static_gas(Gas(5_000_000_000_000))
                     .after_burn(amount),
             )
-    }
-}
-
-#[near_bindgen]
-impl Authorization for Contract {
-    fn add_authorized_account(&mut self, account_id: AccountId) {
-        self.assert_authorized();
-    
-        if !self.get_authorized_accounts().contains(&account_id) {
-            self.authorized_accounts.insert(&account_id);
-        }
-    }
-
-    fn remove_authorized_account(&mut self, account_id: AccountId) {
-        self.assert_authorized();
-
-        self.authorized_accounts.remove(&account_id);
-    }
-
-    fn get_authorized_accounts(&self) -> Vec<AccountId> {
-        let mut result = self.authorized_accounts.to_vec();
-        result.push(env::current_account_id());
-
-        result
     }
 }
 
