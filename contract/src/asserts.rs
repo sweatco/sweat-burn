5377ccfd82e5dfcdd0043bd9077159e3f2000b26
Vasily Styagov <styagov.dev@gmail.com>
2023-07-04 14:59:26 +0100

    Remove contract account id from assertion

--- contract/src/asserts.rs
+++ contract/src/asserts.rs
@@ -4,9 +4,8 @@ use crate::Contract;
 
 impl Contract {
     pub(crate) fn assert_authorized(&self) {
-        let predecessor = env::predecessor_account_id();
         assert!(
-            predecessor == env::current_account_id() || predecessor == self.manager_account_id,
+            env::predecessor_account_id() == self.manager_account_id,
             "Unathorized"
         );
     }
